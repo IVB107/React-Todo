a9e1c5c7ed641c8b44e2339f6ba49d0763171cdb
IVB107 <ianvb107@gmail.com>
2019-02-13 14:54:11 -0600

    First commit, setup App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+  // Event Handlers
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo App</h2>
+        <div className="form-container">
+
+        </div>
       </div>
     );
   }
